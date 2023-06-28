ffb711c00ab87beb51c43363a9f3741c53743105
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-06-28 10:55:03 -0500

    feat: :sparkles: ayudas

---
+++ uploads/maineado.php
@@ -0,0 +1,36 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    require "../vendor/autoload.php";
+    $dotenv = Dotenv\Dotenv::createImmutable("../")->load();
+    $router = new \Bramus\Router\Router();
+
+    $router->mount('/campers', function() use($router){
+        $router->post('/', '\App\crub@postAll');
+        $router->put('/', '\App\crub@putAll');
+        $router->get("/", '\App\crub@getAll');
+        $router->delete('/', function() { 
+            $cox = new \App\connect();
+            $_DATA = json_decode(file_get_contents("php://input"));
+            $res = $cox->con->prepare("DELETE FROM person WHERE DNI=:dni");
+            $res->bindParam(':dni', $_DATA->dni);
+            $res->execute();
+            print_r($res->rowCount());
+        });
+    });
+
+    
+
+
+
+
+/**
+ * https://github.com/bramus/router
+ * https://github.com/vlucas/phpdotenv
+ */
+
+
+
+
+
+    $router->run();
+?>
